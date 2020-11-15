38e72e90bd2f654da30adb41eec5bc8972dd0a0e
Julian Lamprea <jalamprea@gmail.com>
2020-11-15 06:21:20 -0500

    Updates

---
+++ precache-manifest.17d63a14111e009eab9bc67e2aa3f624.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b091045f4b902e67bc6e4a669ef05e03",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6c2e5e7fe45e70afbf83",
+    "url": "./static/css/main.2298e89f.chunk.css"
+  },
+  {
+    "revision": "f198486d57efa1b562b4",
+    "url": "./static/js/2.c861f28c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c861f28c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c2e5e7fe45e70afbf83",
+    "url": "./static/js/main.3e9e7908.chunk.js"
+  },
+  {
+    "revision": "693c7dc6ec7eaea5ce06",
+    "url": "./static/js/runtime-main.6523ec24.js"
+  }
+]);
